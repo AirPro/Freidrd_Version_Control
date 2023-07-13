a8e694bc309d83b2329cf32ffbd6b42dc2d6ceba
Bob Freid <17238974+AirPro@users.noreply.github.com>
2023-07-12 22:23:19 -0400

    Initial Commit push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Freidrd_Version_Control.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
